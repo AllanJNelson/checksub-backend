c1ec97dfd45080b78a92c31701ef5195275b107b
Passion <passion@Passions-iMac.local>
2021-08-06 15:51:31 +0800

    update0806

---
+++ test.py
@@ -0,0 +1,6 @@
+
+
+
+a = 'adfadfadfddd&#;123wde'
+b = a.replace('&#39;', '"')
+print(b)
--- to_auto_srt.py
+++ to_auto_srt.py
@@ -10,13 +10,14 @@ def main(xml_path, target_srt_path):
     global index
     index = 0
     for item in root:
+        st = item.text.replace("&#39;", "'")
         with open(target_srt_path, "a+") as file:
             start = get_time(int(float(item.attrib['start']) * 1000))
             try:
                 end = get_time(int(float(root[index + 1].attrib['start']) * 1000))
             except:
                 end = get_time(int(float(item.attrib['start']) * 1000 + float(item.attrib['dur']) * 1000))
-            file.write(f'{index+1}\n{start} --> {end}\n{item.text}\n\n')
+            file.write(f'{index+1}\n{start} --> {end}\n{st}\n\n')
         index = index + 1
 
 
@@ -26,4 +27,4 @@ def get_time(current):
     mins = f'0{(buf_sec % 3600) // 60}'
     secs = f'0{(buf_sec % 3600) % 60}'
     millisecs = f'000{current % 1000}'
-    return f'{hours}:{mins[-2:]}:{secs[-2:]}, {millisecs[-3:]}'
+    return f'{hours}:{mins[-2:]}:{secs[-2:]},{millisecs[-3:]}'
